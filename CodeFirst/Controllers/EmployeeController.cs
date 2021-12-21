297df2a9574a7aec7cbbb69139304df8b8774e50
Kaveen-Madhusanka <56001178+Kaveen-Madhusanka@users.noreply.github.com>
2021-12-21 22:43:10 +0530

    Complete CRUD With Code first

--- CodeFirst/Controllers/EmployeeController.cs
+++ CodeFirst/Controllers/EmployeeController.cs
@@ -2,6 +2,9 @@
 using CodeFirst.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CodeFirst.Controllers
@@ -32,5 +35,83 @@ namespace CodeFirst.Controllers
                 return NotFound();
             }
         }
+        [HttpGet("GetEmployees")]
+        public async Task<IActionResult> GetEmployees()
+        {
+            try
+            {
+              var Employees =  await _AppDbContext.Employees.Select(s => new Employee()
+                {
+                    iD = s.iD,
+                    Name = s.Name,
+                    Address = s.Address,
+                    Contact = s.Contact
+                }).ToListAsync<Employee>();
+
+                return Ok(Employees);
+            }
+            catch (Exception)
+            {
+
+                return NotFound();
+            }
+        }
+
+        [HttpPut("UpdateEmployee")]
+        public async Task<IActionResult> UpdateEmployee(Employee employee)
+        {
+            try
+            {
+                var ExsitingEmployee = await _AppDbContext.Employees.Where(x=> x.iD == employee.iD).FirstOrDefaultAsync<Employee>();
+                if (ExsitingEmployee != null)
+                {
+                    ExsitingEmployee.Name = employee.Name;
+                    ExsitingEmployee.Address = employee.Address;
+                    ExsitingEmployee.Contact = employee.Contact;
+
+                    _AppDbContext.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpDelete("DeleteEmployee")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                    return NotFound();
+
+                var ExsistingEmploye = await _AppDbContext.Employees.Where(x => x.iD == id).FirstOrDefaultAsync<Employee>();
+                if (ExsistingEmploye != null)
+                {
+                    _AppDbContext.Entry(ExsistingEmploye).State = EntityState.Deleted;
+                    _AppDbContext.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+                    
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
     }
+
 }
